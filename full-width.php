68a226839a747458d4135a64c058640c34cc710d
angeleaquilina1 <64836282+angeleaquilina1@users.noreply.github.com>
2021-05-12 17:59:28 +0200

    Add files via upload

---
+++ full-width.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Full Width Template
+*/
+get_header(); ?>
+
+	<div id="container" class="<?php bravada_get_layout_class(); ?>">
+
+		<main id="main" class="main">
+			<?php cryout_before_content_hook(); ?>
+
+			<?php get_template_part( 'content/content', 'page' ); ?>
+
+			<?php cryout_after_content_hook(); ?>
+		</main><!-- #main -->
+
+		<?php bravada_get_sidebar(); ?>
+
+	</div><!-- #container -->
+
+<?php get_footer();
